d6458aaf650099801626077a247167a9bbbfa651
Fadl Matar <fadlmatar00@gmail.com>
2019-09-29 18:39:07 +0200

    Anden

--- src/Spil/spil.java
+++ src/Spil/spil.java
@@ -2,9 +2,34 @@ package Spil;
 
 public class spil {
     private final int Max = 6;
-    private int facevalue;
+    private int faceValue;
 
     //konstruktør
-    public spil(int value);
+    public spil(int value)
+    {
+        faceValue = value;
+    }
 
+    public int roll()
+    {
+        faceValue = (int)(Math.random()*Max+1);
+
+        return faceValue;
+    }
+
+    public void setFaceValue (int value)
+    {
+        faceValue = value;
+    }
+
+    public int getFaceValue()
+    {
+        return faceValue;
+    }
+
+    public toString()
+    {
+        String result = "øjne: " + faceValue;
+        return result;
+    }
 }
